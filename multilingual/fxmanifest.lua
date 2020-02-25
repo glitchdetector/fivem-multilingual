afc1a8153df7a4cd4101626f4edc6702965adcac
Vegard Bendiktsen <glitchdetector@gmail.com>
2020-02-25 14:08:18 +0100

    Initial commit

---
+++ multilingual/fxmanifest.lua
@@ -0,0 +1,14 @@
+name 'Multilingual'
+author 'glitchdetector'
+contact 'glitchdetector@gmail.com'
+
+fx_version 'adamant'
+game 'gta5'
+
+description 'Support for multiple client languages without using RAGE formats'
+usage [[
+    Check the instructions in the /files/ readme.
+    Add the language file as `language_json` and `file` entries
+]]
+
+client_script 'scripts/cl_*.lua'
